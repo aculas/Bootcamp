404271064e3ac15e320101ecc6105b3a2623dffa
aculas <Jason_hull@ymail.com>
2021-05-29 22:43:24 -0700

    Added base files

---
+++ Python/python_oo_practice/serial.py
@@ -0,0 +1,39 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start=0):
+        """Makes a new serial generator starting at start """
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Shows representation of the generator"""
+        return f"(Serial Generator start = {self.start} next = {self.next})"
+
+    def generate(self):
+        """Returns the next serial number"""
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Resets number to the original start value"""
+        
+        self.next = self.start
