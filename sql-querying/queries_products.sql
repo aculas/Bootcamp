7d0d872cb1a9d4c9570def32bfced64441b37ce0
aculas <Jason_hull@ymail.com>
2021-06-28 15:52:11 -0700

    updated queries

--- sql-querying/queries_products.sql
+++ sql-querying/queries_products.sql
@@ -1,5 +1,3 @@
--- Comments in SQL Start with dash-dash --
-
 -- 1. Add a product to the table with the name of "chair", 
 --  price of 44.00, and can_be_returned of false.
 
@@ -7,3 +5,62 @@ INSERT INTO products
   (name, price, can_be_returned)
 VALUES
   ('chair', 44.00, 'f');
+
+--2. Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
+
+INSERT INTO products
+  (name, price, can_be_returned)
+VALUES
+  ('stool', 25.99, 't');
+
+--3. Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
+
+INSERT INTO products
+  (name, price, can_be_returned)
+VALUES
+  ('table', 124.00, 'f');
+
+--4. Display all of the rows and columns in the table.
+
+SELECT * FROM products;
+
+--5. Display all of the names of the products.
+
+SELECT name FROM products;
+
+--6. Display all of the names and prices of the products.
+
+SELECT name, price FROM products;
+
+--7. Add a new product - make up whatever you would like!
+
+
+
+--8. Display only the products that can_be_returned.
+
+
+
+--9. Display only the products that have a price less than 44.00.
+
+
+--10. Display only the products that have a price in between 22.50 and 99.99.
+
+
+
+
+--11. There’s a sale going on: Everything is $20 off! Update the database accordingly.
+
+
+
+
+--12. Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
+
+
+
+
+--13. And now the sale is over. For the remaining products, increase their price by $20.
+
+
+
+
+--14. There is a new company policy: everything is returnable. Update the database accordingly.
