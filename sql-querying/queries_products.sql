be8a387442f9484ad08bc19f8e9680210251e9c2
aculas <Jason_hull@ymail.com>
2021-06-28 16:00:30 -0700

    updated query

--- sql-querying/queries_products.sql
+++ sql-querying/queries_products.sql
@@ -33,34 +33,34 @@ SELECT name FROM products;
 SELECT name, price FROM products;
 
 --7. Add a new product - make up whatever you would like!
-
-
+INSERT INTO products
+  (name, price, can_be_returned)
+VALUES
+  ('desk', 214.00, 't');
 
 --8. Display only the products that can_be_returned.
 
-
+SELECT * FROM products WHERE can_be_returned;
 
 --9. Display only the products that have a price less than 44.00.
 
+SELECT * FROM products WHERE price < 44.00;
 
 --10. Display only the products that have a price in between 22.50 and 99.99.
 
-
-
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
 --11. There’s a sale going on: Everything is $20 off! Update the database accordingly.
 
-
-
+UPDATE products SET price = price -20;
 
 --12. Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
 
-
-
+DELETE FROM products WHERE price < 25;
 
 --13. And now the sale is over. For the remaining products, increase their price by $20.
 
-
-
+UPDATE products SET price = price + 20;
 
 --14. There is a new company policy: everything is returnable. Update the database accordingly.
+UPDATE products SET can_be_returned = 't';
