11779653d8707b8efe20d96c90b39bbf0c3d1b2d
Jason Hull <Jason_hull@ymail.com>
2021-06-30 20:42:56 -0700

    Add files via upload
    Added base files

---
+++ sql-joins/queries.sql
@@ -0,0 +1,9 @@
+
+--1. Join the two tables so that every column and record appears, regardless of if there is not an owner_id.
+
+
+
+
+
+
+--2. Count the number of cars for each owner. Display the owners first_name, last_name and count of vehicles. The first_name should be ordered in ascending order.
