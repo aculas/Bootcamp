e88c1f9b4ee4df11dea331f5a943cb28bc461b6d
aculas <jason_hull@ymail.com>
2021-08-22 16:56:17 -0700

    updated files and folders

--- Javascript Promises Exercises/promises/part-1/app.js
+++ Javascript Promises Exercises/promises/part-1/app.js
@@ -2,13 +2,13 @@ let favNumber = 5;
 let baseURL = "http://numbersapi.com";
 
 // 1.
-$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+$.getJSON(`${baseURL}/${favNumber}?json`).then((data) => {
   console.log(data);
 });
 
 // 2.
 let favNumbers = [7, 11, 22];
-$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+$.getJSON(`${baseURL}/${favNumbers}?json`).then((data) => {
   console.log(data);
 });
 
@@ -17,6 +17,6 @@ Promise.all(
   Array.from({ length: 4 }, () => {
     return $.getJSON(`${baseURL}/${favNumber}?json`);
   })
-).then(facts => {
-  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+).then((facts) => {
+  facts.forEach((data) => $("body").append(`<p>${data.text}</p>`));
 });
