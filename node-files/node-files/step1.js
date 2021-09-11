a4e78501be88b0f666dc575ee8d6fc06cf0e761c
aculas <jason_hull@ymail.com>
2021-09-10 17:33:35 -0700

    added new file

---
+++ node-files/node-files/step1.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const process = require("process");
+
+// reads file at a certain path and prints it out
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
---
+++ node-files/node-files/step2.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const process = require("process");
+const axios = require("axios");
+
+// reads file at a certain path and prints it out
