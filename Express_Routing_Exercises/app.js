722bfad018cf2cb15a920113a8df19d40d79e4ae
aculas <jhull97212@gmail.com>
2021-09-14 18:12:46 -0700

    updated files

--- Express_Routing_Exercises/app.js
+++ Express_Routing_Exercises/app.js
@@ -12,7 +12,10 @@ const {
 //This will Print the median / average to the console.
 app.get("/mean", function (req, res, next) {
   if (!req.query.num) {
-    throw new ExpressError("That is not a valid number", 400);
+    throw new ExpressError(
+      "You must pass a query key of nums with a comma-separated list of numbers.",
+      400
+    );
   }
   let numsAsString = req.query.nums.split(",");
   let nums = convertAndValidateNumsArray(numsAsString);
@@ -27,12 +30,55 @@ app.get("/mean", function (req, res, next) {
 
   return res.send(result);
 });
+
 app.get("/median", (req, res, next) => {
+  if (!req.query.nums) {
+    throw new ExpressError(
+      "You must pass a query key of nums with a comma-separated list of numbers.",
+      400
+    );
+  }
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+  let result = {
+    operation: "median",
+    result: findMedian(nums),
+  };
+
   return res.send(result);
 });
 
-app.get("/mode", (req, res, next) => {
-  return res.send("Mode is: " + mode);
+app.get("/mode", function (req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError(
+      "You must pass a query key of nums with a comma-separated list of numbers.",
+      400
+    );
+  }
+  let numsAsStrings = req.query.nums.split(",");
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums),
+  };
+
+  return res.send(result);
+});
+
+/** general error handler */
+
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+
+  // pass the error to the next piece of middleware
+  return next(err);
 });
 
 // Error handler
