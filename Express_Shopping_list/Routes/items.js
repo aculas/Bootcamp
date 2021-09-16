b23952288e35e5f01b1838b881b318291cfbdcbc
aculas <jhull97212@gmail.com>
2021-09-15 21:21:14 -0700

    updated routes

--- Express_Shopping_list/Routes/items.js
+++ Express_Shopping_list/Routes/items.js
@@ -1,48 +1,55 @@
-const Item = require('../item');
+const Item = require("../item");
 const express = require("express");
 
 const router = express.Router();
 
+// GET / item returns [item, ...]
 
-router.get("/", function (req, res) {
-  res.json({ items });
-});
-
-router.post("/", function (req, res, next) {
+router.get("", (req, res, next) => {
   try {
-    if (!req.body.name) throw new ExpressError("Name is required", 400);
-    const new items = { name: req.body.name };
-    items.push(newitems);
-    return res.status(201).json({ items: newitems });
+    return res.json({ items: Item.findAll() });
   } catch (e) {
     return next(e);
   }
 });
 
-router.get("/:name", function (req, res) {
-  const founditems = items.find((items) => items.name === req.params.name);
+//  POST / {name, price} returns a new-item
+router.post("", (req, res, next) => {
+  try {
+    let newItem = new Item.find(req.body.name, req.body.price);
+    return res.json({ item: newItem });
+  } catch (e) {
+    return next(e);
+  }
+});
+//  GET /[name] returns item
+router.get("/:name", (req, res) => {
+  const founditems = Item.find((items) => items.name === req.params.name);
   if (founditems === undefined) {
     throw new ExpressError("Item not found", 404);
   }
   res.json({ items: founditems });
 });
+// PATCH /[name] returns item
 
-router.patch("/:name", function (req, res) {
-  const founditems = items.find((items) => items.name === req.params.name);
-  if (founditems === undefined) {
-    throw new ExpressError("items not found", 404);
+router.patch("/:name", (req, res, next) => {
+  try {
+    let foundItem = Item.update(req.params.name, req.body);
+    return res.json({ item: foundItem });
+  } catch (e) {
+    return next(e);
   }
-  founditems.name = req.body.name;
-  res.json({ items: founditems });
 });
 
-router.delete("/:name", function (req, res) {
-  const founditems = itemss.findIndex((items) => items.name === req.params.name);
-  if (founditems === -1) {
-    throw new ExpressError("item not found", 404);
+// DELETE /[name] returns "Deleted"
+
+router.delete("/:name", (req, res, next) => {
+  try {
+    Item.remove(req.params.name);
+    return res.json({ message: "Deleted" });
+  } catch (e) {
+    return next(e);
   }
-  items.splice(founditems, 1);
-  res.json({ message: "Deleted" });
 });
 
 module.exports = router;
