f659b74e14de224f1747504f52c3d3d1d8c2e58f
aculas <jhull97212@gmail.com>
2021-09-15 22:29:10 -0700

    updated tests

--- Express_Shopping_list/Routes/items.test.js
+++ Express_Shopping_list/Routes/items.test.js
@@ -8,76 +8,80 @@ let items = require("../fakeDb");
 
 let item = { name: "food", price: 300 };
 
-beforeEach(function () {
+beforeEach(async () => {
   items.push(item);
 });
 
-afterEach(function () {
+afterEach(async () => {
   // make sure this *mutates*, not redefines,
   items.length = 0;
 });
+
 // Get/Items and returns the object {items: [item, ...]}
-describe("GET /items", async () => {
+
+describe("GET /items", () => {
   test("Get a list of all items", async () => {
-    const res = await request(app).get("/items");
-    const { items } = response.body;
+    const res = await request(app).get(`/items`);
+    const { items } = res.body;
     expect(res.statusCode).toBe(200);
     expect(items).toHaveLength(1);
   });
 });
+
 // Get/ items/[name] - this returns data about 1 item {item: item}
-describe("GET /items/:name", async () => {
+
+describe("GET /items/:name", () => {
   test("Get items by name", async () => {
-    const res = await request(app).get(`/items/${items.name}`);
+    const res = await request(app).get(`/items/${item.name}`);
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ items: pickles });
+    expect(res.body.item).toEqual(item);
   });
-  test("Responds with 404 for invalid items", async () => {
+
+  test("Responds with 404 for invalid item", async () => {
     const res = await request(app).get(`/items/0`);
     expect(res.statusCode).toBe(404);
   });
 });
+
 // POST /items - create items from data
-describe("POST /items", async () => {
+
+describe("POST /items", () => {
   test("Creates a new item", async () => {
     const res = await request(app)
-      .post("/items")
+      .post(`/items`)
       .send({ name: "Cheese", price: 0 });
     expect(res.statusCode).toBe(200);
-    expect(response.body.item).toHaveProperty("name");
-    expect(response.body.item).toHaveProperty("price");
-    expect(response.body.item.name).toEqual("Cheese");
-    expect(response.body.item.price).toEqual(0);
-  });
-  test("Responds with 400 if item is missing", async () => {
-    const res = await request(app).post("/items").send({});
-    expect(res.statusCode).toBe(400);
+    expect(res.body.item).toHaveProperty("name");
+    expect(res.body.item).toHaveProperty("price");
+    expect(res.body.item.name).toEqual("Cheese");
+    expect(res.body.item.price).toEqual(0);
   });
 });
 
 // PATCH /items [name] -updates item and returns {item: item}
-describe("/PATCH /items/:name", async () => {
-  test("Updating a items' name", async () => {
-    const res = await request(app)
-      .patch(`/itemss/${item.name}`)
-      .send({ name: "Sloth" });
+
+describe("/PATCH /items/:name", () => {
+  test("Updates a single item", async () => {
+    const res = await request(app).patch(`/items/${item.name}`).send({
+      name: "Sloth",
+    });
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ items: { name: "Monster" } });
+    expect(res.body.item).toEqual({
+      name: "Sloth",
+    });
   });
   test("Responds with 404 if it can't find item", async () => {
-    const res = await request(app).patch(`/itemss/0`).send({ name: "Sloth" });
+    const res = await request(app).patch(`/items/0`);
     expect(res.statusCode).toBe(404);
   });
 });
+
 // DELETE/items[name] deletes an item and returns {message: item deleted}
-describe("/DELETE /item/:name", async () => {
-  test("Deleting a item", async () => {
+
+describe("/DELETE /item/:name", () => {
+  test("Deletes a single item", async () => {
     const res = await request(app).delete(`/items/${item.name}`);
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({ message: "Deleted" });
-  });
-  test("Responds with 404 for deleting invalid items", async () => {
-    const res = await request(app).delete(`/items/car`);
-    expect(res.statusCode).toBe(404);
   });
 });
