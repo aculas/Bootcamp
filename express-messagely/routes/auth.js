9df42a37bbd5e4c17f41a700e0ab26ac6042dfdc
aculas <jhull97212@gmail.com>
2021-09-27 18:28:29 -0700

    updated auth route

--- express-messagely/routes/auth.js
+++ express-messagely/routes/auth.js
@@ -1,9 +1,51 @@
+/** Routes for demonstrating authentication in Express. */
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
+const {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+} = require("../middleware/auth");
+
+router.get("/", (req, res, next) => {
+  res.send("Welcome!!");
+});
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
 
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError("Username and password required", 400);
+    }
+    const results = await db.query(
+      `SELECT username, password 
+         FROM users
+         WHERE username = $1`,
+      [username]
+    );
+    const user = results.rows[0];
+    if (user) {
+      if (await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username }, SECRET_KEY);
+        return res.json({ message: `Logged in!`, token });
+      }
+    }
+    throw new ExpressError("Invalid username/password", 400);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +53,30 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError("Username and password required", 400);
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    // save to db
+    const results = await db.query(
+      `
+        INSERT INTO users (username, password)
+        VALUES ($1, $2)
+        RETURNING username`,
+      [username, hashedPassword]
+    );
+    return res.json(results.rows[0]);
+  } catch (e) {
+    if (e.code === "23505") {
+      return next(
+        new ExpressError("Username taken. Please pick another!", 400)
+      );
+    }
+    return next(e);
+  }
+});
