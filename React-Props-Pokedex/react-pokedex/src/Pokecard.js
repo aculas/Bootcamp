c552f89b3092779d274b9c715a749f661ac2423a
aculas <jhull97212@gmail.com>
2021-10-02 18:51:08 -0700

    added base files

---
+++ React-Props-Pokedex/react-pokedex/src/Pokecard.js
@@ -0,0 +1,23 @@
+import react from "react";
+import "./Pokecard.css";
+
+const POKE_API =
+  "https://raw.githubusercontent.com/" +
+  "PokeAPI/sprites/master/sprites/pokemon/";
+
+// Single Pokemon card
+
+function Poekcard(props) {
+  let imgSrc = `${POKE_API}${props.id}.png`;
+
+  return (
+    <div className="Pokecard">
+      <div className="Pokecard-title">{props.name}</div>
+      <img className="Pokecard-image" src={imgSrc} />
+      <div className="Pokecard-data">{props.type}</div>
+      <div className="Pokecard-data">{props.exp}</div>
+    </div>
+  );
+}
+
+export default Pokecard;
