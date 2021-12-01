783312467bde6035e53f6971b4c9a6d6b3757395
aculas <jhull97212@gmail.com>
2021-12-01 14:51:53 -0800

    added files and folders

---
+++ react-jobly/api.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class JoblyApi {
+  // the token for interactive with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
+    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+    const params = (method === "get")
+        ? data
+        : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get details on a company by handle. */
+
+  static async getCompany(handle) {
+    let res = await this.request(`companies/${handle}`);
+    return res.company;
+  }
+
+  
+  static async getJobs(searchTerm) {
+    let res = await JoblyApi.request(`jobs?search=${searchTerm}`);
+    return res.jobs;
+  }
+
+  static async getCompanies(searchTerm) {
+    let res = await JoblyApi.request(`companies?search=${searchTerm}`);
+    return res.companies;
+  }
+
+  static async login(username, password) {
+    let res = await JoblyApi.request(`login`, { username, password }, 'post');
+    console.log('login', res);
+    return res.token;
+  }
+
+  static async register(username, password, email, first_name, last_name) {
+    let res = await JoblyApi.request(
+      `users`,
+      { username, password, email, first_name, last_name },
+      'post'
+    );
+    return res.token;
+
+  // obviously, you'll add a lot here ...
+}
+
+// for now, put token ("testuser" / "password" on class)
+JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
