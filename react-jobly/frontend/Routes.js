fce2ec676b8eb45fe692ceab166f75acdb69daaf
aculas <jhull97212@gmail.com>
2021-12-01 16:24:59 -0800

    fixed routes

--- react-jobly/frontend/Routes.js
+++ react-jobly/frontend/Routes.js
@@ -1,54 +1,64 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import NotFound from "./NotFound";
-import Home from "./Home";
-import Companies from "./Companies";
-import Jobs from "./Jobs";
-import Profile from "./Profile";
-import LoginSignup from "./LoginSignup";
-import Company from "./Company";
-import Logout from "./Logout";
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Homepage from "../homepage/Homepage";
+import CompanyList from "../companies/CompanyList";
+import JobList from "../jobs/JobList";
+import CompanyDetail from "../companies/CompanyDetail";
+import LoginForm from "../auth/LoginForm";
+import ProfileForm from "../profiles/ProfileForm";
+import SignupForm from "../auth/SignupForm";
+import PrivateRoute from "./PrivateRoute";
 
-class Routes extends Component {
-  render() {
-    const { updateUserDetails, loginUser, logOutUser } = this.props;
-    return (
+/** Site-wide routes.
+ *
+ * Parts of site should only be visitable when logged in. Those routes are
+ * wrapped by <PrivateRoute>, which is an authorization component.
+ *
+ * Visiting a non-existant route redirects to the homepage.
+ */
+
+function Routes({ login, signup }) {
+  console.debug(
+    "Routes",
+    `login=${typeof login}`,
+    `register=${typeof register}`
+  );
+
+  return (
+    <div className="pt-5">
       <Switch>
-        <Route exact path="/" render={() => <Home />} />
-        <Route exact path="/companies" render={() => <Companies />} />
-        <Route
-          exact
-          path="/companies/:handle"
-          render={(rtProps) => (
-            <Company {...rtProps} updateCurrUser={updateUserDetails} />
-          )}
-        />
-        <Route
-          exact
-          path="/jobs"
-          render={() => <Jobs updateCurrUser={updateUserDetails} />}
-        />
-        <Route
-          exact
-          path="/profile"
-          render={() => <Profile updateCurrUser={updateUserDetails} />}
-        />
-        <Route
-          exact
-          path="/login"
-          render={(rtProps) => (
-            <LoginSignup {...rtProps} loginUser={loginUser} />
-          )}
-        />
-        <Route
-          exact
-          path="/logout"
-          render={() => <Logout logOutUser={logOutUser} />}
-        />
-        <Route render={() => <NotFound />} />
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+
+        <Route exact path="/login">
+          <LoginForm login={login} />
+        </Route>
+
+        <Route exact path="/signup">
+          <SignupForm signup={signup} />
+        </Route>
+
+        <PrivateRoute exact path="/companies">
+          <CompanyList />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/jobs">
+          <JobList />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/companies/:handle">
+          <CompanyDetail />
+        </PrivateRoute>
+
+        <PrivateRoute path="/profile">
+          <ProfileForm />
+        </PrivateRoute>
+
+        <Redirect to="/" />
       </Switch>
-    );
-  }
+    </div>
+  );
 }
 
 export default Routes;
