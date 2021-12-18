6d7778f28e5b1f39d363e56640324f2a9321182d
aculas <jhull97212@gmail.com>
2021-12-18 15:33:28 -0800

    updated folders and files

---
+++ Microblog/microblog/Front-End/src/components/NewPost.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./NewPost.css";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { sendPostToAPI } from "../actions/posts";
+import PostForm from "../components/PostForm";
+
+// Shows posts form and handles the editing of the form.
+
+function NewPost () {
+
+const dispatch = useDispatch();
+const history = useHistory();
+
+// Adds posts and saves to the backend.
+
+function add ({title, description, body}) {
+    dispatch(sendPostToAPI(title, description, body));
+    history.push("/");
+}
+
+
+// Cancels pist (redirect)
+
+  function cancel() {
+    history.push("/");
+  }
+
+  return (
+    <main>
+      <h1>New Post</h1>
+      <PostForm save={add} cancel={cancel} />
+    </main>
+  );
+}
+
+export default NewPost;
+
+
+
+
+
+
+
+
+
+
+
+}
