94099b4541856c37b828223a581b4254a5bc2e2f
aculas <jhull97212@gmail.com>
2022-01-05 14:22:15 -0800

    updated methods

--- dsa-arrays-linked-lists/linked-list.js
+++ dsa-arrays-linked-lists/linked-list.js
@@ -37,7 +37,33 @@ class LinkedList {
 
   /** unshift(val): add new value to start of list. */
 
-  unshift(val) {}
+  unshift(val) {
+    //creates a new node with the val passed
+    const newNode = new Node(val);
+    //if there is no head property on the list, set the head and tail to be the new node
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      //set the new node's next property to the current head
+      newNode.next = this.head;
+      //reassign head property to be the new node
+      this.head = newNode;
+    }
+    //increment the list length by 1
+    this.length++;
+  }
+
+  _get(idx) {
+    let cur = this.head;
+    let count = 0;
+
+    while (cur !== null && count != idx) {
+      count += 1;
+      cur = cur.next;
+    }
+
+    return cur;
+  }
 
   /** pop(): return & remove last item. */
 
@@ -61,11 +87,56 @@ class LinkedList {
 
   /** removeAt(idx): return & remove item at idx, */
 
-  removeAt(idx) {}
+  removeAt(idx) {
+    if (idx >= this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    // special case: remove first item
+
+    if (idx === 0) {
+      let val = this.head.val;
+      this.head = this.head.next;
+      this.length -= 1;
+      if (this.length < 2) this.tail = this.head;
+      return val;
+    }
+
+    let prev = this._get(idx - 1);
+
+    // special case: remove tail
+
+    if (idx === this.length - 1) {
+      let val = prev.next.val;
+      prev.next = null;
+      this.tail = prev;
+      this.length -= 1;
+      return val;
+    }
+
+    // normal case: removes in the middle
+
+    let val = prev.next.val;
+    prev.next = prev.next.next;
+    this.length -= 1;
+    return val;
+  }
 
   /** average(): return an average of all values in the list */
 
-  average() {}
+  average() {
+    if (this.length === 0) return 0;
+
+    let total = 0;
+    let current = this.head;
+
+    while (current) {
+      total += current.val;
+      current = current.next;
+    }
+
+    return total / this.length;
+  }
 }
 
 module.exports = LinkedList;
