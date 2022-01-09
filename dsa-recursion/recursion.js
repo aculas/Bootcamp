aae7b910fb308db2bd297bbf33c9cd861a66368e
aculas <jhull97212@gmail.com>
2022-01-08 17:30:39 -0800

    updated functions

--- dsa-recursion/recursion.js
+++ dsa-recursion/recursion.js
@@ -1,51 +1,56 @@
 /** product: calculate the product of an array of numbers. */
 
-function product(nums) {
-
+function product(nums, idx = 0) {
+  if (idx === nums.length) return 1;
+  return nums[idx] * product(nums, idx + 1);
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
-function longest(words) {
-
+function longest(words, idx = 0, longestWord = 0) {
+  if (idx === words.length) return longestWord;
+  longestWord = Math.max(words[idx].length, longestWord);
+  return longest(words, idx + 1, longestWord);
 }
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {
-
+function everyOther(str, idx = 0, newStr = "") {
+  if (idx >= str.length) return newStr;
+  newStr += str[idx];
+  return everyOther(str, idx + 2, newStr);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
-
+function isPalindrome(str, idx = 0) {
+  let leftIdx = idx;
+  let rightIdx = str.length - idx - 1;
+  if (leftIdx >= rightIdx) return true;
+  if (str[leftIdx] !== str[rightIdx]) return false;
+  return isPalindrome(str, idx + 1);
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+function findIndex(arr, val, idx = 0) {
+  if (idx === arr.length) return -1;
+  if (arr[idx] === val) return idx;
+  return findIndex(arr, val, idx + 1);
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-
-}
+function revString(str) {}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
-}
+function gatherStrings(obj) {}
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
-
-}
+function binarySearch(arr, val) {}
 
 module.exports = {
   product,
@@ -55,5 +60,5 @@ module.exports = {
   findIndex,
   revString,
   gatherStrings,
-  binarySearch
+  binarySearch,
 };
