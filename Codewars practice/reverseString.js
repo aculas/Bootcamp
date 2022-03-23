29ef58bd29b0e3c19eb21850809bdbf79ea77287
aculas <jason_hull@ymail.com>
2022-03-23 14:02:26 -0700

    fixed text

--- Codewars practice/reverseString.js
+++ Codewars practice/reverseString.js
@@ -10,5 +10,17 @@ function wordReverse(str) {
 }
 
 // length - 1 is last element in index
-// First in last out menthod
+// First in last out method ex: a,b,c,d will return d,c,b,a
 // an arr starts off in order when pushed in a stack then popped off it is backwards
+
+// Similar approach using a arrow function and reverses in place
+const reverseString = (str) => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+};
+
+reverseString("hello world!");
+// => !dlrow olleh
