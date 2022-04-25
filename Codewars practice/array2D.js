3b42e6b91026233f4a07596dae17159b7b98ff5f
aculas <jason_hull@ymail.com>
2022-04-25 11:05:15 -0700

    added exercises

---
+++ Codewars practice/array2D.js
@@ -0,0 +1,30 @@
+function minimumDistance(area) {
+  // Set empty counter
+  let distance = 0;
+  // Create a 1d array
+  let array = new Array(3);
+  // Loops pver 1d array to create 2d array
+  for (let i = 0; i < array.length; i++) {
+    array[i] = [];
+    for (let j = 0; j < area.length; j++) {
+      area += distance;
+    }
+  }
+}
+
+function minimumDistance(area) {
+  // starts off with counter variable
+  let distance = [];
+
+  //   Loops over the length of the area of a 2d array
+  for (let i = 0; i < area.length; i++) {
+    let myObj = {};
+    //   Nested loop that loops over the elements inside the first array
+    for (let j = 0; j < area[i].length; j++) {
+      // sets the values
+      myObj[distance[i][j][0]] = area[i][j][1];
+      distance.push(myObj);
+    }
+  }
+  return distance;
+}
