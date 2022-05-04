c4c2101a40baee635e0accd47ff6bb59557f8b0e
aculas <jason_hull@ymail.com>
2022-05-04 15:06:08 -0700

    updated missing exercise

--- Codewars practice/arrayCountEven.js
+++ Codewars practice/arrayCountEven.js
@@ -1,1 +1,11 @@
 // take an array and return the even number of elements in the array
+function countEven(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      return arr[i];
+    }
+    return arr;
+  }
+}
+
+countEven([1, 2, 3, 4, 5, 6]);
